7484b94e9b47dda24b7d4b6dddf946fdaf63836b
Himanshu Chaurasia <himanshuchaurasia015@gmail.com>
2025-04-04 18:09:22 +0530

    h,w added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ function App() {
   const [clicked, setClicked] = useState(false);
   const handleClick = () => {
     setClicked(!clicked);
-    alert("button clicked");
+    // alert("button clicked");
   };
   return (
     <>
       <div className="fixed bottom-5 right-5 z-50">
-        <div className={!clicked ? "hidden" : "w-[100px] h-[100px]"}></div>
+        <div
+          className={!clicked ? "hidden" : "w-[100px] h-[100px] bg-red-200"}
+        ></div>
         <button
           onClick={handleClick}
           className="w-16 h-16 rounded-full bg-blue-600 text-white text-3xl shadow-lg hover:bg-blue-700 transition"
